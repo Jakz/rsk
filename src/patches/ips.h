9bfe8de69dab4e70731882dd0bfc7a0749f61856
Jack <jack@pixbits.com>
2017-05-07 16:38:16 +0200

    added catch test library, created patches folder, starting working on universal biendian numbers

---
+++ src/patches/ips.h
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  CSOTool
+//
+//  Created by Jack on 12/03/17.
+//  Copyright © 2017 Jack. All rights reserved.
+//
+
+#include "common.h"
+#include "path.h"
+
+struct IPSEntry
+{
+  off_t
+};
+
+class IPSPatch
+{
+  std::vector<IPSEntry> entries;
+};
